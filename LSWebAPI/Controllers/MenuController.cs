ce4cf891dc84d14dda9a8b73445706823926622b
licovali <vlicovali@gmail.com>
2014-04-11 19:47:07 -0500

    Menu try

---
+++ LSWebAPI/Controllers/MenuController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LSWebAPI.Models;
+namespace LSWebAPI.Controllers
+{
+    public class MenuController : ApiController
+    {
+        root[] menu = new root[]
+        {
+            new root { 
+                id = "1", 
+                text = "menu1", 
+                iconCls = "menu_admin", 
+                parent_id = null,  
+                className = null, 
+                leaf = false, 
+                items = new items[] { 
+                    new items { id = "2", text = "menu11", iconCls = "menu_groups", parent_id = "1",  className = "panel", leaf = true },
+                    new items { id = "3", text = "menu12", iconCls = "menu_groups", parent_id = "1",  className = "panel", leaf = true } 
+                } 
+            }
+        };
+
+        public IEnumerable<root> Get(int id)
+        {
+            return menu;
+        }
+    }
+}
---
+++ LSWebAPI/Models/items.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LSWebAPI.Models
+{
+    public class items
+    {
+        public string id { get; set; }
+        public string text { get; set; }
+        public string iconCls { get; set; }
+        public string parent_id { get; set; }
+        public string className { get; set; }
+        public bool leaf { get; set; }
+    }
+
+    public class root
+    {
+        public string id { get; set; }
+        public string text { get; set; }
+        public string iconCls { get; set; }
+        public string parent_id { get; set; }
+        public string className { get; set; }
+        public bool leaf { get; set; }
+        public items[] items { get; set; }
+    }
+}
