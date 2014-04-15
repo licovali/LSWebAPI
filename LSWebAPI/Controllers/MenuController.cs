bec7614a844fbc9f65689e27a49194547f9fa1e5
licovali <vlicovali@gmail.com>
2014-04-15 14:55:44 -0500

    Menu changes
    Minor changes to menu

--- LSWebAPI/Controllers/MenuController.cs
+++ LSWebAPI/Controllers/MenuController.cs
@@ -18,15 +18,15 @@ namespace LSWebAPI.Controllers
         root[] menu = new root[]
         {
             new root { 
-                id = "1", 
-                text = "menu1", 
-                iconCls = "menu_admin", 
+                id = "0", 
+                text = "<i class=\"fa fa-shield\"></i>" + " Security", 
+                iconCls = "", 
                 parent_id = null,  
                 className = null, 
                 leaf = false, 
                 items = new items[] { 
-                    new items { id = "2", text = "menu11", iconCls = "menu_groups", parent_id = "1",  className = "panel", leaf = true },
-                    new items { id = "3", text = "menu12", iconCls = "menu_groups", parent_id = "1",  className = "panel", leaf = true } 
+                    new items { id = "1", text = "Groups", iconCls = "", parent_id = "1",  className = "panel", leaf = true },
+                    new items { id = "2", text = "Users", iconCls = "", parent_id = "1",  className = "panel", leaf = true } 
                 } 
             }
         };
